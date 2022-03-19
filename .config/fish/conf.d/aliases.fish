ce0df721cef4713cac21646d94fe151c1d8555b1
Alexander Netrusov <github@netrusov.me>
2022-03-20 00:25:24 +0300

    add fish docker functions

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -2,3 +2,15 @@ alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 alias dk='docker'
 alias dkc='docker-compose'
+
+function dk-rm
+  docker container rm (docker container ls -f 'status=exited' -q)
+end
+
+function dk-rmi
+  docker image rm (docker image ls -f 'dangling=true' -q)
+end
+
+function dk-rmv
+  docker volume rm (docker volume ls -f 'dangling=true' -q)
+end
