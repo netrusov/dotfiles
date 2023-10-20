7381366a093e902c4ff19ec2d1acd8d4d4962564
Alexander Netrusov <github@netrusov.me>
2023-10-20 15:24:38 +0300

    set path in main config

--- .config/fish/conf.d/local-bin.fish
+++
@@ -1,3 +0,0 @@
-if test -d ~/.local/bin
-  set --export PATH "$HOME/.local/bin:$PATH"
-end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,9 @@
 set --export EDITOR vim
 
+if test -d ~/.local/bin
+  set --export PATH "$HOME/.local/bin:$PATH"
+end
+
 if status is-interactive
   set -g fish_prompt_pwd_dir_length 0
 end
